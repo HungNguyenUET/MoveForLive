d87da4ff0be146c94645535c28bee2c558cecfce
nguyenquocviet1306 <nguyenquocviet1306@gmail.com>
2016-08-25 17:00:01 +0700

    doi hinh qua

--- src/Model/Gift.java
+++ src/Model/Gift.java
@@ -4,8 +4,8 @@ package Model;
  * Created by Minh on 8/24/2016.
  */
 public class Gift extends GameObject {
-    public static final int WIDTH = 60;
-    public static final int HEIGHT = 60;
+    public static final int WIDTH = 40;
+    public static final int HEIGHT = 40;
 
     public Gift(int x, int y, int width, int height) {
         super(x, y, width, height, 0, 0);
